fdf9400758f840204bc9bcf9c1ebf412c5cb3ff5
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-19 12:42:08 +0530

    REFACTORED:Function Added Condition For Day And ADDED:Constants

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -6,18 +6,23 @@ echo "Welcome To Gambling Simulator"
 #CONSTANTS
 BET=1;
 WIN=1;
+WINNING_CONDITION=150;
+LOSSING_CONDITION=50;
 
 #VARIABLE
 stake=100;
 
-#CHECKING FOR CONDITION
+#FUNCTION TO CHECK CONDITION AND PLAY
 function gamble() {
-	if [[ $(( RANDOM%2 )) -eq $WIN ]]
-	then
-		stake=$(($stake+$BET))
-	else
-		stake=$(($stake-$BET))
-	fi
+	while [[ $stake -gt $LOSSING_CONDITION && $stake -lt $WINNING_CONDITION ]]
+	do
+		if [[ $(( RANDOM%2 )) -eq $WIN ]]
+		then
+			stake=$(($stake+$BET))
+		else
+			stake=$(($stake-$BET))
+		fi
+	done
 }
 
 #CALLING FUNCTION
